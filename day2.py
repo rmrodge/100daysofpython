4b09f4814314e1a5c20109a40681bb7f94535d80
Bobby Rodgers <bobby.rodgers@mailfence.com>
2022-02-23 09:20:31 -0500

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first=two_digit_number[0]
+second=two_digit_number[1]
+
+first_int=int(first)
+second_int=int(second)
+
+print(first + " + " + second)
+sum=first_int + second_int
+print(sum)
